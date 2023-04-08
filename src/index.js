37727d6e4ebfe3bef7e9d0416b037b0ceee14f81
RinaBoronina <111557018+RinaBoronina@users.noreply.github.com>
2023-04-08 13:12:15 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
+/* 
+const A = Math.PI
+export A - отправить
+
+import A - получить
+*/
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// root.render(React.createElement('h1', { title: 'dogs' }, 'Hello React!'));
+// то на JSX
+root.render(
+    <div>
+        <h1>Hello React!</h1>
+        <p>DogFood shop</p>
+    </div>
+);
